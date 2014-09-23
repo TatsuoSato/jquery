512bb9a1530a8d8d12d9369884c7725f3e5852b9
佐藤 龍生 <tatsuosato0908@gmail.com>
2014-09-24 01:41:16 +0900

    jQueryトレーニング(その4)まで完了
    jQueryトレーニング(その4)P72まで完了

--- before/c01/scripts/custom.js
+++ before/c01/scripts/custom.js
@@ -1,8 +1,18 @@
 $(function(){
-    //<p>がクリックされたらアウトラインを付ける  
-    $('p').on('click', function(){
+    //<p>がマウスオーバーされたらアウトラインを付ける  
+    $('p').on('mouseenter', function(){
         $(this).css('outline', '1px solid gold');
+    })
+    .on('mouseleave',function(){
+        $(this).css('outline', 'none');
     });
+    $('input[type=text]').on('focus',function(){
+        $(this).css('background-color', 'orange');
+    })
+    .on('blur', function(){
+        $(this).css('background-color', 'transparent');
+    });
+    /*
     $('ul').css('background-color', 'yellowgreen');
     $('#ordered').css('background-color', 'wheat');
     $('.external').css('text-decoration', 'none');
@@ -10,4 +20,5 @@ $(function(){
     $('tr:nth-child(even)').css('background-color', 'lightblue');
     $('input[type=text]').css('background-color', 'orange');
     $('h1, h2').css('color', 'steelblue');
+    */
 });
