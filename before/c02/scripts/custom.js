7b5ab16018435c9d7eb81bd38a975c671eb68d74
佐藤 龍生 <tatsuosato0908@gmail.com>
2014-10-06 01:19:22 +0900

    jQueryトレーニング第2章(02-03)まで完了
    jQueryトレーニング第1章(02-03)P118まで完了

--- before/c02/scripts/custom.js
+++ before/c02/scripts/custom.js
@@ -23,3 +23,28 @@ $(function(){
         $('.nav-global').slideToggle(400);
     });
 });
+$(function(){
+    $('.tab-menu')
+    .on('click','li > a', function(event){
+        event.preventDefault();
+        var $this = $(this);
+
+        //ボタンをアピアランスを変更する
+        $this.parent().siblings()
+        .removeClass('selected')
+        .end()
+        .addClass('selected');
+
+        //コンテンツを切り換える
+        var tabId = $this.data('tabid');
+        $this.closest('.tab').find('.tab-contents').children()
+        .each(function(){
+            var $content = $(this);
+            if($content.data('contentid') == tabId){
+                $content.removeClass('hidden');
+            } else {
+                $content.addClass('hidden');
+            }
+        });
+    })
+});
