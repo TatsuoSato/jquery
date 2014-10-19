42e15ab6ed66ee64c718688c31662ad7d868d0bc
佐藤 龍生 <tatsuosato0908@gmail.com>
2014-10-20 03:51:06 +0900

    jQueryトレーニング第3章(03-01)まで完了
    jQueryトレーニング第3章(03-01)P138まで完了

---
+++ before/c03/scripts/custom.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.accordion')
+    .on('click', 'li', function(){
+        var $this = $(this);
+        //コンテンツを開く
+        $this
+        .toggleClass('expanded')
+        .children('.content')
+        .slideToggle('fast')
+        .end()
+        .siblings()
+        .removeClass('expanded')
+        .children('.content')
+        .slideUp('fast');
+    });
+});
