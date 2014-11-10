f2f1eb98ddbbe886bfa33edc5d6be95bed40713e
佐藤 龍生 <tatsuosato0908@gmail.com>
2014-11-11 02:06:01 +0900

    jQueryトレーニング第3章(03-05)まで完了
    jQueryトレーニング第3章(03-05)P176まで完了

--- before/c03/scripts/custom.js
+++ before/c03/scripts/custom.js
@@ -55,3 +55,55 @@ $(function(){
         .removeClass('animate');
     });
 });
+$(function(){
+    $('.second')
+    .on('mouseenter', '.btn-action', function(event){
+        event.preventDefault();
+        $(this).find('img')
+        .animate({'opacity':0.5}, 300)
+        .animate({'opacity':1}, 500)
+    })
+    .on('mouseleave', 'btn-action', function(event){
+        event.preventDefault();
+        $(this).find('img')
+        .animate({'opacity':1}, 300);
+    });
+});
+$(function(){
+    var value = 0.5;
+
+    $('.third')
+    .on('mouseenter mouseleave', '.btn-action', function(event){
+        event.preventDefault();
+        var opacityValue;
+        if(event.type == 'mouseenter'){
+            opacityValue = value;
+        } else {
+            opacityValue = 1;
+        }
+
+        $(this).find('img')
+        .animate(
+            {opacity:opacityValue},
+        {
+            duration:500,
+            step:function(now, tween){
+                var rotate = 'rotate(' + (1 - now) * (360 / value) +'deg)';
+                $(this)
+                .css({
+                    '-webkit-transform': rotate,
+                    'transform' : rotate
+                });
+            }
+        });
+    })
+    .each(function(){
+        try {
+            event = document.createEvent('TouchEvent');
+            $(this)
+            .off('mouseenter')
+            .off('mouseleave');
+        } catch(err){
+        }
+    });
+});
